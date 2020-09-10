bca1ef19b4b20a98789fc0a08e91fd92ee85230d
Erick Castille <ecastille924@gmail.com>
2020-09-10 23:52:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-
+  if phrase !== phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elseif phrase == phrase.upcase 
+    return "NO, NOT SINCE 1938!"
+  elseif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
 end
